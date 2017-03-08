f96a8adf4ce1c886b485ce6af4fe320af96fbc20
Sepehr Eghbali <sepehr3pehr@gmail.com>
2017-03-07 22:01:26 -0500

    Update types.h

--- include/types.h
+++ include/types.h
@@ -1,9 +1,4 @@
-/*
- * types.h
- *
- *  Created on: 2016-03-09
- *      Author: s2eghbal
- */
+
 
 #ifndef TYPES_H_
 #define TYPES_H_
@@ -21,12 +16,6 @@ typedef unsigned char UINT8;
 
 typedef double REAL;
 
-/* typedef uint64_t UINT64; */
-/* typedef uint32_t UINT32; */
-/* typedef uint16_t UINT16; */
-/* typedef uint8_t UINT8; */
-
-// Constants
 #define UINT64_1 ((UINT64)0x01)
 #define UINT32_1 ((UINT32)0x01)
 
